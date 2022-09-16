e3e6117a5b3014d495c240ee0c7614f7a87d6dea
MatheusPinhalLacerra <matheusplacerra@gmail.com>
2022-09-16 20:53:05 -0300

    Ajuste tela Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,144 +1,112 @@
 <!DOCTYPE html>
-
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boolean Cursos</title>
 
-  <style>
-    body {
-      margin: 0;
-      padding: 0;
-    }
-
-
-    #menu ul {
-      margin: 0;
-      background-color: #1E2C72;
-      list-style: none;
-    }
-
-    #menu ul li {
-      display: inline;
-    }
-
-    #menu ul li a {
-      padding: 10px 30px;
-      display: inline-block;
-      color: white;
-      text-decoration: none;
-
-    }
-
-    #menu ul li a:hover {
-      color: #3552E2;
-      background-color: white;
-    }
-
-    #cabecalho{
-      background: #3552E2;
-    }
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="/js/script.js"></script>
     
-    #cabecalho ul {
-      list-style: none;
-      text-align: end;
-      height: 100%;
-    }
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    #cabecalho ul li {
-      display: inline;
-    }
 
-    #cabecalho ul li a {
+    <link rel="stylesheet" type="text/css" href="/css/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="/css/slick/slick-theme.css" />
 
-      padding: 10px 30px;
-      display: inline-block;
-      color: white;
-      text-decoration: none;
-
-    }
-
-    #cabecalho ul li a:hover {
-      color: #3552E2;
-      background-color: white;
-    }
-  </style>
-
+</head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="padding: 20px 0">
+        <div class="container">
+            <div class="row" id="row-navbar">
 
-  <div id="cabecalho">
-    
-  <img src="/img/logo.png" width="300" height="100" class="d-inline-block align-top" alt="">
-    
-  
-      <ul>
-        <li><a href="{{route('login')}}">Login</a></li>
-        <li><a href="{{ route('register') }}">Register</a></li>
-      </ul>
+                <div class="col-7">
+                    <a class="navbar-brand" href="#"><img src="/img/logo.png" alt="Logo Boolean Cursos"
+                            id="logo-welcome"></a>
+                </div>
+
+                <div class="col-5" style="display: grid" id="itens-navbar">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><b id="item-login">Fazer Login</b></a>
+                            </li>
+                            <li class="nav-item" id="item-cadastro">
+                                <a class="nav-link disabled" href="#"><b id="item-cadastro-b">Cadastrar-se</b></a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="/img/carrossel1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="/img/carrossel1.jpg" alt="Second slide">
+            </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 
+    {{-- Função nova --}}
 
-  <div id="menu">
-    <ul>
-      <li><a href="#">Inicio</a></li>
-      <li><a href="#">Cursos</a></li>
-      <li><a href="#">Quem somos</a></li>
-  </div>
-  </ul>
-  </div>
+    <div >
+        <h1>test</h1>
+    </div>
 
-</body>
+    <div class="your-class">
+        <div><img src="/img/logo.png" alt=""></div>
+        <div><img src="/img/logo.png" alt=""></div>
+    </div>
+
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+   
 
 
-
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
-
-<body class="antialiased">
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-
-
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" ;>
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-
-
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="/img/carrossel1.jpg" alt="First slide" style="max-width: 40%; height: auto" ;>
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="/img/carrossel2.jpg" alt="Second slide" style="max-width: 40%;height: auto" ;>
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="/img/carrossel3.jpg" alt="Third slide" style="max-width: 40%;height: auto" ;>
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-
-
-    </html>
